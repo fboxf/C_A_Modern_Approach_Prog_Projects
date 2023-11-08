75ea682a467665dd229d07e6c4a3d610a6495c25
fboxf <148175236+fboxf@users.noreply.github.com>
2023-11-08 14:25:23 +0100

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,18 @@
+#include <stdio.h> 
+#define PI 3.14159
+
+float sphere_volume (float);
+
+int main (void) 
+{
+	float radius = 10.00;
+
+	printf("%f", sphere_volume(radius));
+		
+	return 0;
+}
+
+float sphere_volume (float radius)
+{
+	return (4.0/3.0)*(PI* (radius * radius * radius));
+}
