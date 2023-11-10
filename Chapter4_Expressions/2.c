3bcbf32dbed607731bd62ebaa27d2996482ef75a
fboxf <148175236+fboxf@users.noreply.github.com>
2023-11-10 23:09:24 +0100

    Update 2.c

--- Chapter4_Expressions/2.c
+++ Chapter4_Expressions/2.c
@@ -2,21 +2,19 @@
 
 int main (void)
 {
-	int digit_to_reverse, left_to_right_digit, right_to_left_digit,
-		middle_digit;
+	int digit_to_reverse, left_to_right_digit, middle_digit, right_to_left_digit;
 
 	printf("Enter a three digit number:");
 	scanf("%d", &digit_to_reverse);
 
 	left_to_right_digit = (digit_to_reverse - (digit_to_reverse % 100)) / 100;
-	right_to_left_digit = (digit_to_reverse % 100) % 10;
 	middle_digit = ((digit_to_reverse % 100) - digit_to_reverse % 10) / 10;
-
+	right_to_left_digit = (digit_to_reverse % 100) % 10;
+	
 	printf("The reversal is: %d%d%d", 
 			right_to_left_digit,
 			middle_digit,
 			left_to_right_digit);
-
 	return 0;
 }
 
