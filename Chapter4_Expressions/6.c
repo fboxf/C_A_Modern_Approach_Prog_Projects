b931f9c78c929563c2b2a2d9d6a5e85540813f4c
fboxf <148175236+fboxf@users.noreply.github.com>
2023-11-11 10:50:07 +0100

    Add files via upload

---
+++ Chapter4_Expressions/6.c
@@ -0,0 +1,26 @@
+#include <stdio.h>	
+
+int main (void)
+{
+	int i1, i2, i3, i4, i5, i6, i7, i8, i9, i10, i11, i12;
+	int first_sum;
+	int second_sum;
+	int check_digit;
+
+	printf("Enter the first 12 digits of an EAN:");
+
+	/* read no */
+	scanf("%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d",
+			&i1, &i2, &i3, &i4, &i5, &i6, &i7, &i8, &i9, &i10, &i11, &i12);
+
+	/* perfrom calc */
+	first_sum = i2 + i4 + i6 + i8 + i10;
+	second_sum = i1 + i3 + i5 + i7 + i9 + i11;
+
+	check_digit = 9 - ((((first_sum * 3) + second_sum) - 1) % 10) ;
+
+	/* output check digit */
+	printf("Check digit: %d \n", check_digit);
+
+	return 0;
+}
