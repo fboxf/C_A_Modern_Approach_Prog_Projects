673fb993e3d24228f02900a3cc96fec9d77d3ee8
fboxf <148175236+fboxf@users.noreply.github.com>
2023-11-12 11:27:20 +0100

    Add files via upload

---
+++ Chapter5_SelectionStatements/4.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int wind_force (float);
+
+int main (void)
+{
+	float spd_kts;
+	
+	printf("Please enter a wind speed (in knots): ");
+	scanf("%f",&spd_kts);
+
+	wind_force(spd_kts);
+	
+	return 0;
+}
+
+int wind_force (float wind_speed)
+{
+	if (wind_speed < 1)
+	{
+		printf("Wind force: Calm\n");
+		return 0;
+	}
+	else if (wind_speed < 4)
+	{
+		printf("Wind force: Light Air\n");
+		return 0;
+	}
+	else if (wind_speed < 27)
+	{
+		printf("Wind force: Breeze\n");
+		return 0;
+	}
+	else if (wind_speed < 47)
+	{
+		printf("Wind force: Gale\n");
+		return 0;
+	}
+	else if (wind_speed < 63)
+	{
+		printf("Wind force: Storm\n");
+		return 0;
+	}
+	else
+	{
+		printf("Wind force: Hurricane\n");
+		return 0;
+	}
+}
+
