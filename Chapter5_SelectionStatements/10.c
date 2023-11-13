0ed3689f83d7dbb1795bf2598f0a8a37447be746
fboxf <148175236+fboxf@users.noreply.github.com>
2023-11-13 20:23:31 +0100

    Update 10.c

--- Chapter5_SelectionStatements/10.c
+++ Chapter5_SelectionStatements/10.c
@@ -2,7 +2,9 @@
 
 int main (void)
 {	
-	int grade, grade_tens, grade_ones;
+	int grade, 
+		grade_tens_digits, 
+		grade_ones_digits;
 
 	printf("Enter numerical grade: ");
 	scanf("%d", &grade);
@@ -13,16 +15,16 @@ int main (void)
 	} 
 	else 
 	{
-		grade_tens = grade / 10;
-		grade_ones = grade % 10;
+		grade_tens_digits = grade / 10;
+		grade_ones_digits = grade % 10;
 
-		switch (grade_tens) {
+		switch (grade_tens_digits) {
 			case 0: case 1: case 2: case 3: case 4: case 5: 
-					printf("%d%d Grade F\n", grade_tens, grade_ones); break;
-			case 6: printf("%d%d Grade D\n", grade_tens, grade_ones); break;
-			case 7: printf("%d%d Grade C\n", grade_tens, grade_ones); break;
-			case 8: printf("%d%d Grade B\n", grade_tens, grade_ones); break;
-			case 9: printf("%d%d Grade A\n", grade_tens, grade_ones); break;
+					printf("%d%d Grade F\n", grade_tens_digits, grade_ones_digits); break;
+			case 6: printf("%d%d Grade D\n", grade_tens_digits, grade_ones_digits); break;
+			case 7: printf("%d%d Grade C\n", grade_tens_digits, grade_ones_digits); break;
+			case 8: printf("%d%d Grade B\n", grade_tens_digits, grade_ones_digits); break;
+			case 9: printf("%d%d Grade A\n", grade_tens_digits, grade_ones_digits); break;
 		}
 	}
 	return 0;
