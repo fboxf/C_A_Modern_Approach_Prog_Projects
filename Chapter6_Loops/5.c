d57658536c9455a9b4d92f819e991b8018aa89f8
fboxf <148175236+fboxf@users.noreply.github.com>
2023-11-14 14:12:57 +0100

    Update 5.c

--- Chapter6_Loops/5.c
+++ Chapter6_Loops/5.c
@@ -4,12 +4,12 @@ int main (void)
 {
 	long int digit_to_reverse;
 
-	printf("Enter an integer:");
+	printf("Enter an integer:"); /* for example 345 */
 	scanf("%ld", &digit_to_reverse);
 
 	do {
-		printf("%ld",digit_to_reverse % 10);
-		digit_to_reverse = digit_to_reverse / 10;
+		printf("%ld",digit_to_reverse % 10); /* First 5 >>>> then 4 >>>> then 3 >>>> end */
+		digit_to_reverse = digit_to_reverse / 10; /* 345 >>>> 34 >>>> 3*/
 	} while (digit_to_reverse > 0);
 
 	return 0;
