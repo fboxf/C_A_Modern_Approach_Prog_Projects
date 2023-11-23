cd09a844035212f2fd9e741134232e5d3d5c2796
fboxf <148175236+fboxf@users.noreply.github.com>
2023-11-23 07:04:00 +0100

    Update 14.c

--- Chapter7_BasicTypes/14.c
+++ Chapter7_BasicTypes/14.c
@@ -1,32 +1,25 @@
 /* Newton's method */
-/*  */
+
 #include <stdio.h>
 #include <math.h>
 int main (void){
 	double x; /* user input */
-	double guess_y_old, guess_y_new; /* initial square root guess */
+	double guess_y_old, guess_y_new;
 
 	printf("Enter a positive number:");
 	scanf("%lf",&x);
-	guess_y_new = 1;
+	guess_y_new = 1; /* initial guess */
 
 	printf("                               Average of\n");
 	printf("x         y         x/y        y and x/y \n");
 	printf("--------------------------------------------\n");
-	/* Program Loop */
-	while( (fabs(guess_y_old - guess_y_new)) > 0.00001){
+	while((fabs(guess_y_old - guess_y_new)) > 0.00001){
 
 		printf("%lf   %lf   %lf     %lf    \n", 
-				x, 
-				guess_y_new, 
-				x/guess_y_new, 
-				(guess_y_new + (x/guess_y_new)/2)
-			  );
-
+				x, guess_y_new, x/guess_y_new, (guess_y_new + (x/guess_y_new)/2));
+		
 		guess_y_old = guess_y_new;
 		guess_y_new = ((guess_y_new + (x/guess_y_new))/2);
-
-
 	}
 	return 0;
 }
