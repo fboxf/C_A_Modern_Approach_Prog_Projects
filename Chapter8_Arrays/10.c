a08440196bddf0c8796821f2433b25bc18ac9958
fboxf <148175236+fboxf@users.noreply.github.com>
2023-11-25 15:08:40 +0100

    Update 10.c

--- Chapter8_Arrays/10.c
+++ Chapter8_Arrays/10.c
@@ -1,9 +1,6 @@
-/* Modify Programming project 8 from chapter 5 so that the
- * departure times are stored in an array and the arrival times 
- * are stored in a second array (The times are integers representing
- * the number of minutes since midnight)
- * The program will use a loop to search the array of departure times for the 
- * one closest to the time entered by the user */
+/* Modify Programming project 8 from chapter 5 so that the departure times are stored in an array and the arrival times 
+ * are stored in a second array (The times are integers representing the number of minutes since midnight)
+ * The program will use a loop to search the array of departure times for the one closest to the time entered by the user */
 
 #include <stdio.h>
 #define HOURS 60
