d624f8c9edba573c8d9fb941ced816e1953fc1fa
fboxf <148175236+fboxf@users.noreply.github.com>
2023-11-28 11:18:40 +0100

    Update 15.c

--- Chapter8_Arrays/15.c
+++ Chapter8_Arrays/15.c
@@ -1,22 +1,9 @@
-/* One of the oldest encryption techniques is the Caesar 
- * cypher, attributed to Julius Casear. It involves 
- * replacing eac hletter in a message with another letter that is
- * a fixed number of positions later in the alphabet.
- *
- * Write a program that encrypts a message using a Casar cipher. The user will
- * enter the message to be encrypted and the shift amount
- *
- * Assumptions: 
- * 		Message doesn't exceed 80 characters. 
- * 		Characters other than letters unchanged.
- * 		lower remains lower/ upper remains upper
- * 		Wrap around ((ch - 'A') + n) % 26 + 'A'
- * 	ch stores letter and n stores shift amount */
+/* Caesar cypher */
 #include <stdio.h>
 #include <ctype.h>
 
 int main (void){
-	char ch, un_en_msg[80], en_msg[80];
+	char ch, un_en_msg[80];
 	int n;
 
 	printf("Enter message to be encrypted: ");
@@ -39,7 +26,6 @@ int main (void){
 			printf("%c",((ch - 'a') + n) % 26 + 'a');
 		} else 
 			printf("%c", ch);
-
 	}
 
 	printf("\n");
