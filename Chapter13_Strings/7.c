a13475b1c98bf21ccb0edf315151cdf725b0a90f
fboxf <148175236+fboxf@users.noreply.github.com>
2024-02-10 09:50:47 +0100

    Add files via upload

--- Chapter13_Strings/7.c
+++ Chapter13_Strings/7.c
@@ -6,16 +6,17 @@ int main (void)
 	printf("Enter a two-digit number: ");
 	scanf("%d",&two_digit_number);
 
-	char ptr_array_tens[8][8] = {"twenty","thirty" ,"forty","fifty","sixty","seventy","eighty","ninety"};
-	char ptr_array_ones[10][6] = {"","one","two","three","four","five","six","seven","eight","nine"};
-	char ptr_eleven_nineteen[9][10] = {"eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"};
+	char *ptr_array_tens[] = {"twenty","thirty" ,"forty","fifty","sixty","seventy","eighty","ninety"};
+	char *ptr_array_ones[] = {"","one","two","three","four","five","six","seven","eight","nine"};
+	char *ptr_eleven_nineteen[] = {"eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"};
+
 	if (two_digit_number > 19)
 	{
 		int tens_digit = two_digit_number / 10;
 		int ones_digit = two_digit_number % 10;
 
 		printf("%s", ptr_array_tens[tens_digit - 2]);
-		printf("-%s\n", ptr_array_ones[ones_digit]);
+		printf(" %s\n", ptr_array_ones[ones_digit]);
 
 	} 
 	else if (two_digit_number > 10)
