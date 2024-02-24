907cd4721bd9391e70fb81d02a396666ba817946
fboxf <148175236+fboxf@users.noreply.github.com>
2024-02-24 23:18:03 +0100

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,27 @@
+/* For brevity, I've removed most of the countries */
+#include <stdio.h>
+
+struct dialing_code {
+	char *country;
+	int code;
+};
+
+const struct dialing_code country_codes[] = {
+	{"Argentina", 54},{"Bangladesh", 880},
+	{"Brazil", 55},{"Burma (Myanmar)", 95}
+};
+
+int main (void){
+	int dialing_code;
+	printf("%s", "Enter an international dialing code:");
+	scanf("%d", &dialing_code);
+	size_t size_of_struct = sizeof(country_codes) / sizeof(country_codes[0]);
+
+	for (int i = 0; i < size_of_struct; i++){
+		if (country_codes[i].code == dialing_code){
+			printf("%s\n",country_codes[i].country);
+		}
+	}
+
+	return 0;
+}
